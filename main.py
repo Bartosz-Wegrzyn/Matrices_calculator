2d4c64d6c8a2ad369234d113b9396d312591abcb
bartoszwegrzyn <Bartoszvvegrzyn@gmail.com>
2021-03-22 19:10:06 +0100

    preparation of all definitions that will be written in this project

--- main.py
+++ main.py
@@ -1,5 +1,59 @@
 # This Python script is a basics matrices calculator, just for my practice peres to understand how matrices works.
 
+### auxiliary definitions ###
+
+def is_identity_matrix():
+    pass
+
+def is_diagonal_matrix():
+    pass
+
+def macierz_zerowa():
+    pass
+
+### Main definicions ###
+
+def matrix_transpose(A):
+    pass
 
 
+def matrix_addition(A, B):
+    pass
 
+
+def matrix_multiplication(A, B):
+    pass
+
+
+def matrix_determinant(A):
+    pass
+
+
+def matrix_trace(A):
+    pass
+
+
+# def matrix_main_minor(A):
+#     pass
+
+
+# def matrix_leading_major_minors(A):
+#     pass
+
+# def matrix_minor(A):
+#     pass
+
+def matrix_algebraic_complements(A, n, m):
+    pass
+
+
+def matrix_algebraic_complement(A):
+    pass
+
+
+def attached_matrix(A):
+    pass
+
+
+def matrix_inverse():
+    pass
