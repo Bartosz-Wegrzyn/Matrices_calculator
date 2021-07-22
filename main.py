848a06d68cdcd28675ae6c00547fdb6bb458039d
Bartosz-Wegrzyn <bartosz.wegrzyn@icloud.com>
2021-07-22 09:46:04 +0200

    det

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
-# This Python script is a basics matrices calculator, just for my practice peres to understand how matrices works.
-
 import numpy as np
+from tests import *
 
 
-### auxiliary definitions ###
-
-def is_identity_matrix():
-    pass
-
-
-def is_diagonal_matrix():
-    pass
+def is_square(A):
+    if A.shape[0] == A.shape[1]:
+        return True
+    else:
+        return False
 
 
-def zero_matrix(A):
-    pass
+def is_identity_matrix(A):
+    if is_square(A) == True:
+        diagonal_index = 0
+        for rows in A:
+            if rows[diagonal_index] != 1 or sum(rows) > 1:
+                return False
+            for elements in rows:
+                if elements != 0 and rows[diagonal_index] != elements:
+                    return False
 
+            diagonal_index += 1
 
-### Main definicions ###
+        return True
+
+    else:
+        return False
+
 
 def matrix_transpose(A):
-    out = np.array()
+    return A.transpose()
 
 
 def matrix_addition(A, B):
@@ -30,82 +38,65 @@ def matrix_addition(A, B):
         return f"ShapeError: A: {A.shape} B: {B.shape}"
 
 
-def matrix_multiplication(A, B):
+def matrixes_multiplication(A, B):
     if A.shape[1] == B.shape[0]:
         return A * B
     else:
-        return f"ShapeError: A: {A.shape} B: {B.shape} \n            {A.shape[1]} != {B.shape[0]}"
+        return f"ShapeError: A: {A.shape[1]} != {B.shape[0]}"
 
 
-def matrix_determinant(A):
-    pass
+def drop(M, col, row):
+    M = np.delete(M, col, 1)
+    M = np.delete(M, row, 0)
+    return M
 
 
-def matrix_trace(A):
-    pass
+def det(A):
+    result = 0
+    if A.shape == (1, 1):
+        return A[0][0]
+    else:
+        for i in range(len(A)):
+            result += (A[0][i] * ((-1) ** (i + 2)) * det(drop(A, i, 0)))
+        return result
 
 
+
+# def matrix_trace(A):
+#     pass
+#
+#
 # def matrix_main_minor(A):
 #     pass
-
-
+#
+#
 # def matrix_leading_major_minors(A):
 #     pass
-
+#
 # def matrix_minor(A):
 #     pass
+#
+# def matrix_algebraic_complements(A, n, m):
+#     pass
+#
+#
+# def matrix_algebraic_complement(A):
+#     pass
+#
+#
+# def attached_matrix(A):
+#     pass
+#
+#
+# def matrix_inverse(A):
+#     pass
+#
+#
+# def is_diagonal_matrix():
+#     pass
+#
+#
+# def is_zero_matrix(A):
+#     pass
 
-def matrix_algebraic_complements(A, n, m):
-    pass
-
-
-def matrix_algebraic_complement(A):
-    pass
-
-
-def attached_matrix(A):
-    pass
-
-
-def matrix_inverse(A):
-    pass
-
-
-### Test arrays ###
-
-A = np.array([[1, 0, 4, 9, 10, 11, 77]])
-
-B = np.array([
-    [0, 2, 3, 2, 12],
-    [0, 4, 1, 1, 2],
-    [0, 32, 1, 11, 2],
-    [0, 4, 2, 11, 1],
-    [12, 0, 4, 3, 9]])
-
-C = np.array([
-    [1, 2],
-    [2, 2],
-    [9, 1],
-    [3, 0]])
-
-D = np.array([
-    [12],
-    [3],
-    [14],
-    [23]])
-
-E = np.array([
-    [1, 0, 0],
-    [0, 1, 0],
-    [0, 0, 1]])
-
-F = np.array([
-    [1, 0, 0],
-    [0, 4, 0],
-    [0, 0, 4]])
-
-G = np.array([
-    [0, 4, 1, 1],
-    [1, 0, 4, 0]])
-
-print(matrix_multiplication(A, C))
+print(det(H))
